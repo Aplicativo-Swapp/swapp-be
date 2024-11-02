c9ddfcec0664d7c44483e03f6b386c8dbef95789
FernandoGT3 <luiz.moloni@gmail.com>
2024-11-02 20:01:49 -0300

    Reestruturação do Backend, e configuração do docker para os microsserviços

--- django/trade_service/trade_service/urls.py
+++ django/trade_service/trade_service/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for config project.
+URL configuration for trade_service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls'))
 ]
