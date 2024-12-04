cfce397db97a66821b983e07a67b76312c5cb7fc
Caio Bonani Carvalho <caio_bonani2002@hotmail.com>
2024-12-04 18:37:09 -0300

    removido spring

--- springboot/inserir_habilidade/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "inserir_habilidade"
--- springboot/inserir_habilidade/src/main/java/com/example/inserir_habilidade/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.inserir_habilidade.repository;
-
-import com.example.inserir_habilidade.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
